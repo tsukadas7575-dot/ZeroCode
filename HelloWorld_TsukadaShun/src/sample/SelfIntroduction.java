3a28a44a6782889defcefc8e04945d9bc6250bb3
tsukadashunsuke <tsukadashunsuke@192.168.0.230>
2026-01-10 18:30:32 +0900

    名前・年齢・趣味の出力を追加

---
+++ HelloWorld_TsukadaShun/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "塚田駿輔";
+		int age = 32;
+		String hobby = "筋トレ";
+
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		
+		System.out.println("私の名前は" +"塚田駿輔"+ "です。");
+		System.out.println("年齢は" + 32+ "歳です。");
+		System.out.println("趣味は" + "筋トレ" + "です。");
+	}
+	
+	
+
+}
