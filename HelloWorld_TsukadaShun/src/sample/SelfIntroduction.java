f87115dc2b47cf952d9c793814cc477bd96dc897
tsukadashunsuke <tsukadashunsuke@192.168.0.230>
2026-01-14 19:06:26 +0900

    Sample（SelfIntroduction）名前：年齢・趣味

--- HelloWorld_TsukadaShun/src/sample/SelfIntroduction.java
+++ HelloWorld_TsukadaShun/src/sample/SelfIntroduction.java
@@ -19,6 +19,7 @@ public class SelfIntroduction {
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "歳です。");
 		System.out.println("趣味は" + hobby  + "です。");
+		
 	}
 	
 	
