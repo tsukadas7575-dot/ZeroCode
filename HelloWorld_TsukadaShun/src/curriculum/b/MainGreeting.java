b56548fc99a0eb76bce7cf144629ca1b184099ba
tsukadashunsuke <tsukadashunsuke@192.168.0.230>
2026-01-18 12:14:28 +0900

    curriculum.b（Question5：MainGreeting ）クラス分割し、importによる処理呼び出しを実装

---
+++ HelloWorld_TsukadaShun/src/curriculum/b/MainGreeting.java
@@ -0,0 +1,12 @@
+package curriculum.b; // 同じパッケージなので import は不要
+
+public class MainGreeting {
+    public static void main(String[] args) {
+
+        Greeting greeting = new Greeting(); // Greetingクラスのインスタンス生成
+        greeting.sayHello();                // sayHelloメソッドを呼び出す
+        
+      
+
+    }
+}
