98c84aec6cbbcfcff2c5e5a7ac1391937ed39efb
tsukadashunsuke <tsukadashunsuke@fe80:0:0:0:b075:c5ff:fe43:f27a%13>
2026-01-22 07:08:07 +0900

    curriculum.e 基本概念（Employee, Main）

---
+++ HelloWorld_TsukadaShun/src/curriculum/e/Employee.java
@@ -0,0 +1,10 @@
+package curriculum.e;
+
+public class Employee {
+
+    String name;
+
+    public void work() {
+        System.out.println(name + "は働いています。");
+    }
+}
---
+++ HelloWorld_TsukadaShun/src/curriculum/e/Main.java
@@ -0,0 +1,11 @@
+package curriculum.e;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+        employee.name = "山田太郎";
+        employee.work();
+    }
+}
