7f8f587d3d903f165aa7d91f4a3d08432f587719
tsukadashunsuke <tsukadashunsuke@fe80:0:0:0:b075:c5ff:fe43:f27a%13>
2026-01-22 07:13:07 +0900

    curriculum.f クラスとオブジェクト（Employee,Main）

---
+++ HelloWorld_TsukadaShun/src/curriculum/f/Employee.java
@@ -0,0 +1,11 @@
+package curriculum.f;
+
+public class Employee {
+
+    String employeeId;
+    String name;
+
+    public void showInfo() {
+        System.out.println("社員ID: " + employeeId + ", 名前: " + name);
+    }
+}
---
+++ HelloWorld_TsukadaShun/src/curriculum/f/Main.java
@@ -0,0 +1,13 @@
+package curriculum.f;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+        employee.employeeId = "E001";
+        employee.name = "佐藤太郎";
+
+        employee.showInfo();
+    }
+}
